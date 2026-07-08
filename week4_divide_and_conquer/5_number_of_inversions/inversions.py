c86033df5428b7ffb0e03400eec8c55dc7d18323
KuoWei Tseng <kuoweitseng@macbookpro.ucalgary.ca>
2026-07-08 13:20:08 -0600

    add inversion solution

--- week4_divide_and_conquer/5_number_of_inversions/inversions.py
+++ week4_divide_and_conquer/5_number_of_inversions/inversions.py
@@ -1,16 +1,33 @@
 from itertools import combinations
 
 
-def inversions_naive(a):
-    number_of_inversions = 0
-    for i, j in combinations(range(len(a)), 2):
-        if a[i] > a[j]:
-            number_of_inversions += 1
-    return number_of_inversions
+def count_inversions(a):
+    if len(a) <= 1:
+        return a, 0
+    mid = len(a) // 2
+    left,  left_inv  = count_inversions(a[:mid])      # LINE A
+    right, right_inv = count_inversions(a[mid:])      # LINE B
+    merged, split_inv = merge_and_count(left, right)  # LINE C
+    return merged, left_inv + right_inv + split_inv
+
+def merge_and_count(left, right):
+    merged = []
+    i = j = 0
+    inversions = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i]); i += 1
+        else:
+            merged.append(right[j]); j += 1
+            inversions += len(left) - i
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged, inversions
 
 
 if __name__ == '__main__':
     input_n = int(input())
     elements = list(map(int, input().split()))
     assert len(elements) == input_n
-    print(inversions_naive(elements))
+    _, ans = count_inversions(elements)
+    print(ans)
