cf5fecaa823a0f3ae239ddfaa2cab7edb3be9f13
KuoWei Tseng <kuowei.tseng@ucalgary.ca>
2026-07-13 17:14:51 -0600

    implement lcs3

--- week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -1,5 +1,15 @@
-def lcs3(first_sequence, second_sequence, third_sequence):
-    return 0
+def lcs3(a, b, c):
+    n, m, q = len(a), len(b), len(c)
+    dp = [[[0] * (q+1) for _ in range(m+1)] for _ in range(n+1)]
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            for k in range(1, q + 1):
+                if a[i-1] == b[j-1] == c[k-1]:
+                    dp[i][j][k] = 1 + dp[i-1][j-1][k-1]       # all three match — extend
+                else:
+                    dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])         # drop from one of the three
+    
+    return dp[n][m][q]
 
 if __name__ == '__main__':
     n = int(input())
