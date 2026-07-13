06a99205dfde6bf2007739d276231b7708cb5de0
KuoWei Tseng <kuowei.tseng@ucalgary.ca>
2026-07-13 14:52:41 -0600

    add primitive cal

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,5 +1,32 @@
 def compute_operations(n):
-    return []
+    min_ops = [0] * (n + 1)
+    parent  = [0] * (n + 1)
+    min_ops[1] = 0
+
+    for m in range(2, n + 1):
+        best = min_ops[m - 1] + 1      # the always-available +1 option
+        prev = m - 1
+
+        if m % 2 == 0 and min_ops[m // 2] + 1 < best:
+            best = min_ops[m // 2] + 1
+            prev = m // 2
+
+        if m % 3 == 0 and min_ops[m // 3] + 1  < best:    # is coming from m//3 better?
+            best = min_ops[m // 3] + 1
+            prev = m//3
+
+        min_ops[m] = best
+        parent[m]  = prev
+
+    sequence = []
+    m = n
+    while m >= 1:
+        sequence.append(m)
+        if m == 1:
+            break
+        m = parent[m]
+    sequence.reverse()
+    return sequence
 
 
 if __name__ == '__main__':
