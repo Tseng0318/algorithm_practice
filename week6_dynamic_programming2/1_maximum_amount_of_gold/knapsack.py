5c698a94931d3b731d9e5da6a46d7fde691f41d2
Kuowei Tseng <kuowei.tseng@ucalgary.ca>
2026-07-20 16:18:17 -0600

    implement knapsack problem

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -2,7 +2,23 @@ from sys import stdin
 
 
 def maximum_gold(capacity, weights):
-    assert False
+    n = len(weights)
+    
+    # dp[i][w] = max weight using first i bars with capacity w
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
+            # option 1: skip bar i
+            dp[i][w] = dp[i-1][w]
+            
+            # option 2: take bar i (only if it fits)
+            if weights[i-1] <= w:
+                take = weights[i-1] + dp[i-1][w - weights[i-1]]
+                if take > dp[i][w]:
+                    dp[i][w] = take
+    
+    return dp[n][capacity]
 
 
 if __name__ == '__main__':
